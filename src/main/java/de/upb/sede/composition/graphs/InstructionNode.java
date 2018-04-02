d0d9fc39c71d8b6ad42d9c7430d490350acab0a7
Manuel Scheibl <scheiblm@mail.uni-paderborn.de>
2018-04-02 02:23:16 +0200

    Just some spelling fixes

--- src/main/java/de/upb/sede/composition/graphs/InstructionNode.java
+++ src/main/java/de/upb/sede/composition/graphs/InstructionNode.java
@@ -11,87 +11,93 @@ import de.upb.sede.exceptions.UnassignedFieldException;
  * 
  * @author aminfaez
  *
- *	This node represents an instruction in a graphComposition.
+ *         This node represents an instruction in a graphComposition.
  */
 public final class InstructionNode extends BaseNode {
-	
+
 	private static final String unassignedValue = "UNDEFINED";
-	
+
 	private String fmInstruction;
-    private String leftsideFieldname;
-    private String host;
-    private String context;
-    private String method;
-    private List<String> inputFieldnames;
-    
+	private String leftsideFieldname;
+	private String host;
+	private String context;
+	private String method;
+	private List<String> inputFieldnames;
+
 	@SuppressWarnings("unchecked")
-	public InstructionNode(String basedOnInstruction) { 
-    		this.fmInstruction = Objects.requireNonNull(basedOnInstruction);
-    		leftsideFieldname = unassignedValue;
-    		host = unassignedValue;
-    		context = unassignedValue;
-    		method = unassignedValue;
-    		inputFieldnames = Collections.EMPTY_LIST;
-    	}
-	
-	
+	public InstructionNode(String basedOnInstruction) {
+		this.fmInstruction = Objects.requireNonNull(basedOnInstruction);
+		leftsideFieldname = unassignedValue;
+		host = unassignedValue;
+		context = unassignedValue;
+		method = unassignedValue;
+		inputFieldnames = Collections.EMPTY_LIST;
+	}
+
 	public String getFmInstruction() {
 		return this.fmInstruction;
 	}
-	
-	
+
 	/*
-	 * stantard get, set, isAssigned methods. 
+	 * standard get, set, isAssigned methods.
 	 */
-	
+
 	public String getLeftSideFieldname() {
-		if(!isAssignedLeftSideFieldname()) {
+		if (!isAssignedLeftSideFieldname()) {
 			throw new UnassignedFieldException(this, "leftSideFieldName");
 		}
 		return leftsideFieldname;
 	}
+
 	public void setLeftSideFieldname(String leftsideFieldname) {
 		this.leftsideFieldname = Objects.requireNonNull(leftsideFieldname);
 	}
+
 	public boolean isAssignedLeftSideFieldname() {
 		return leftsideFieldname != unassignedValue;
 	}
 
 	public String getHost() {
-		if(!isAssignedHost()) {
+		if (!isAssignedHost()) {
 			throw new UnassignedFieldException(this, "host");
 		}
 		return host;
 	}
+
 	public void setHost(String host) {
 		this.host = Objects.requireNonNull(host);
 	}
+
 	public boolean isAssignedHost() {
 		return host != unassignedValue;
 	}
-	
+
 	public String getContext() {
-		if(!isAssignedContext()) {
+		if (!isAssignedContext()) {
 			throw new UnassignedFieldException(this, "context");
 		}
 		return context;
 	}
+
 	public void setContext(String context) {
 		this.context = Objects.requireNonNull(context);
 	}
+
 	public boolean isAssignedContext() {
 		return context != unassignedValue;
 	}
-	
+
 	public String getMethod() {
-		if(!isAssignedMethod()) {
+		if (!isAssignedMethod()) {
 			throw new UnassignedFieldException(this, "method");
 		}
 		return method;
 	}
+
 	public void setMethod(String method) {
 		this.method = Objects.requireNonNull(method);
 	}
+
 	public boolean isAssignedMethod() {
 		return method != unassignedValue;
 	}
@@ -99,9 +105,9 @@ public final class InstructionNode extends BaseNode {
 	public List<String> getInputFieldnames() {
 		return inputFieldnames;
 	}
+
 	public void setInputFieldnames(List<String> inputFieldNames) {
-		inputFieldnames =  Collections.unmodifiableList(inputFieldNames);
+		inputFieldnames = Collections.unmodifiableList(inputFieldNames);
 	}
-    	
-    
+
 }

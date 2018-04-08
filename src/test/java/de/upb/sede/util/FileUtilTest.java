ae4ff5416725b5a45667e96fc3d2f9b34cc249b5
Amin F <aminfaez@mail.upb.de>
2018-04-08 14:07:18 +0200

    Completed test for readFileAsString utility method

--- src/test/java/de/upb/sede/util/FileUtilTest.java
+++ src/test/java/de/upb/sede/util/FileUtilTest.java
@@ -2,8 +2,11 @@ package de.upb.sede.util;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class FileUtilTest {
 	@Test public void test_readFileAsString() {
-		
+		String testFilePath = "testrsc/FileUtil.readFileAsString_Testfile.txt";
+		Assert.assertEquals("A\nB\nC\nD",FileUtil.readFileAsString(testFilePath));
 	}
 }

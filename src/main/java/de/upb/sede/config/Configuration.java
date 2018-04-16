56c0837f9e39f72a3fcc2ea0cc741fdfbdeeb696
Manuel Scheibl <scheiblm@mail.uni-paderborn.com>
2018-04-16 17:03:32 +0200

    Update on InstructionNode (Reflection)

--- src/main/java/de/upb/sede/config/Configuration.java
+++ src/main/java/de/upb/sede/config/Configuration.java
@@ -1,5 +1,5 @@
 package de.upb.sede.config;
 
 public class Configuration {
-    
+	
 }
--- src/main/java/de/upb/sede/procedure/InstructionNodeProcedure.java
+++ src/main/java/de/upb/sede/procedure/InstructionNodeProcedure.java
@@ -5,12 +5,17 @@ import java.lang.reflect.Method;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.Attributes;
 
 import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
 
+import de.upb.sede.exec.ExecutionEnvironment;
+import de.upb.sede.exec.SEDEObject;
 import de.upb.sede.exec.Task;
 
 public class InstructionNodeProcedure implements Procedure {
@@ -18,39 +23,55 @@ public class InstructionNodeProcedure implements Procedure {
 
 	@Override
 	public Object process(Task task) {
-		String method = (String) task.getParameters().get("method");
-		boolean isConstructor = (boolean) task.getParameters().get("is-service-construction");
-		String leftsidefieldname = (String) task.getParameters().get("leftsidefieldname");
-		String host = (String) task.getParameters().get("host");
-		String context = (String) task.getParameters().get("context");
-		boolean isContextAFieldname = (boolean) task.getParameters().get("is-context-a-fieldname");
-		List<String> parameters = (List<String>) task.getParameters().get("params");
-		
-		Class[] parameterClasses = getClassesForParameters(parameters);
-		
+		ExecutionEnvironment environment = task.getExecution().getExecutionEnvironment();
+		InstructionNodeAttributes attributes = new InstructionNodeAttributes(task);
 		try {
-			Class reflectedClass = loadClass(context);
-			if(isConstructor) {
-				Constructor constructor = reflectedClass.getConstructor(parameterClasses);
+			Map<String, SEDEObject> parameterObjects = getParameterObjects(attributes.getParameters(), environment); 
+			Class<?>[] parameterClasses = getParameterClasses(parameterObjects);
+			
+			List<Object> parameterValues = getParameterValues(parameterObjects);
+			Class<?> contextClass = addClassInClassLoader(attributes.getContext());
+			if (attributes.isConstructor()) {
+				Constructor<?> constructor = contextClass.getConstructor(parameterClasses);
+				constructor.newInstance(parameterValues);
+			} else {
+
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 		return null;
 
 	}
 
-	private Class[] getClassesForParameters(Map<String, Object> parameters) throws ClassNotFoundException {
-		Class[] parameterClasses = new Class[parameters.size()];
-		for(String className : parameters.keySet()) {
-			Class parameterClass = Class.forName(className);
-		}
-		
+	private Map<String, SEDEObject> getParameterObjects(List<String> parameters, ExecutionEnvironment environment) {
+		Map<String, SEDEObject> result = new LinkedHashMap<>(parameters.size());
+		parameters.forEach(varName -> result.put(varName, environment.get(varName)));
+		return result;
 	}
 
-	private Class loadClass(String context) throws Exception {
+	private Class<?>[] getParameterClasses(Map<String, SEDEObject> parameterObjects) throws ClassNotFoundException {
+		List<Class<?>> inOrderClasses = new ArrayList<>(parameterObjects.size());
+		for (SEDEObject sedeObject : parameterObjects.values()) {
+			String classType = sedeObject.getType();
+			Class<?> clazz = Class.forName(classType);
+			inOrderClasses.add(clazz);
+		}
+		Class<?>[] inOrderClassesArray = new Class<?>[inOrderClasses.size()];
+		inOrderClassesArray = inOrderClasses.toArray(inOrderClassesArray);
+		return inOrderClassesArray;
+	}
+
+	private List<Object> getParameterValues(Map<String, SEDEObject> parameterObjects) {
+		List<Object> inOrderObjects = new ArrayList<>(parameterObjects.size());
+		for (SEDEObject sedeObject : parameterObjects.values()) {
+			inOrderObjects.add(sedeObject.getObject());
+		}
+		return inOrderObjects;
+	}
+
+	private Class<?> addClassInClassLoader(String context) throws Exception {
 		try {
 			URL serviceFilesURL = new URL("jar", "", context + "!/");
 			JarURLConnection jarURLConnection = (JarURLConnection) serviceFilesURL.openConnection();
@@ -62,7 +83,7 @@ public class InstructionNodeProcedure implements Procedure {
 			}
 			URLClassLoader urlClassLoader = new URLClassLoader(new URL[] { serviceFilesURL },
 					this.getClass().getClassLoader());
-			Class reflectedClass = Class.forName(context, true, urlClassLoader);
+			Class<?> reflectedClass = Class.forName(context, true, urlClassLoader);
 			addURLToClassLoader(serviceFilesURL);
 			return reflectedClass;
 		} catch (Exception e) {
@@ -82,5 +103,61 @@ public class InstructionNodeProcedure implements Procedure {
 		} else {
 			logger.error("System ClassLoader is no URLClassLoader.");
 		}
+	}
+
+	class InstructionNodeAttributes {
+		private final boolean isConstructor;
+		private final boolean isContextAFieldname;
+		private final String method;
+		private final String leftsidefieldname;
+		private final String host;
+		private final String context;
+		private final String fmInstruction;
+		private final List<String> parameters;
+
+		public InstructionNodeAttributes(Task task) {
+			Map<String, Object> parameters = task.getParameters();
+			this.isConstructor = (boolean) parameters.get("is-service-construction");
+			this.isContextAFieldname = (boolean) parameters.get("is-context-a-fieldname");
+			this.method = (String) parameters.get("method");
+			this.leftsidefieldname = (String) parameters.get("leftsidefieldname");
+			this.host = (String) parameters.get("host");
+			this.context = (String) parameters.get("context");
+			this.fmInstruction = (String) parameters.get("fmInstruction");
+			this.parameters = (List<String>) parameters.get("params");
+		}
+
+		public String getFmInstruction() {
+			return fmInstruction;
+		}
+
+		public boolean isConstructor() {
+			return isConstructor;
+		}
+
+		public boolean isContextAFieldname() {
+			return isContextAFieldname;
+		}
+
+		public String getMethod() {
+			return method;
+		}
+
+		public String getLeftsidefieldname() {
+			return leftsidefieldname;
+		}
+
+		public String getHost() {
+			return host;
+		}
+
+		public String getContext() {
+			return context;
+		}
+
+		public List<String> getParameters() {
+			return parameters;
+		}
+
 	}
 }

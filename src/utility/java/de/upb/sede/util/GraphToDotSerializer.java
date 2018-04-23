a677b094eb0b08b891fe437430cc6f7b9559522f
Manuel Scheibl <scheiblm@mail.uni-paderborn.de>
2018-04-23 19:05:43 +0200

    Clean up of DOT-file serializer

--- src/utility/java/de/upb/sede/util/GraphToDotSerializer.java
+++ src/utility/java/de/upb/sede/util/GraphToDotSerializer.java
@@ -1,8 +1,5 @@
 package de.upb.sede.util;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import de.upb.sede.composition.graphs.CompositionGraph;
 import de.upb.sede.composition.graphs.Edge;
 import de.upb.sede.composition.graphs.GraphTraversal;
@@ -14,31 +11,17 @@ import info.leadinglight.jdot.enums.Shape;
 public class GraphToDotSerializer {
 
 	public static String getDOTForGraph(CompositionGraph compGraph) {
-		Set<BaseNode> alreadySeenBaseNodes = new HashSet<>();
 		Graph graph = new Graph();
+		for (BaseNode baseNode : GraphTraversal.iterateNodes(compGraph)) {
+			Node dotNode = new Node(baseNode.toString()).setShape(Shape.circle);
+			graph.addNode(dotNode);
+		}
 		for (Edge edge : GraphTraversal.iterateEdges(compGraph)) {
 			BaseNode fromNode = edge.getFrom();
 			BaseNode toNode = edge.getTo();
-			if (!alreadySeenBaseNodes.contains(fromNode)){
-				Node dotFromNode = new Node(fromNode.toString()).setShape(Shape.circle);
-				graph.addNode(dotFromNode);
-				alreadySeenBaseNodes.add(fromNode);
-			}
-			if (!alreadySeenBaseNodes.contains(toNode)) {
-				Node dotToNode = new Node(toNode.toString()).setShape(Shape.circle);
-				graph.addNode(dotToNode);
-				alreadySeenBaseNodes.add(toNode);
-			}
 			info.leadinglight.jdot.Edge dotEdge = new info.leadinglight.jdot.Edge();
 			dotEdge.addNode(fromNode.toString()).addNode(toNode.toString());
 			graph.addEdge(dotEdge);
-		}
-		//Adding those nodes that are adjacent to no edge.
-		for(BaseNode baseNode : GraphTraversal.iterateNodes(compGraph)) {
-			if(!alreadySeenBaseNodes.contains(baseNode)){
-				Node dotNode = new Node(baseNode.toString()).setShape(Shape.circle);
-				graph.addNode(dotNode);
-			}
 		}
 		return graph.toDot();
 	}

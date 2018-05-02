571ebf5ad83eb60d6291d392d57fef6a3460ac8d
Amin F <aminfaez@mail.upb.de>
2018-05-02 09:22:13 +0200

    Executor: Added task observer-pattern

--- src/main/java/de/upb/sede/exec/Task.java
+++ src/main/java/de/upb/sede/exec/Task.java
@@ -1,12 +1,16 @@
 package de.upb.sede.exec;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-public final class Task {
+public final class Task implements TaskObserver {
 	private final Execution execution;
 	private final String taskName;
 	private final Map<String, Object> attributes;
+
+	private List<TaskObserver> observers = new ArrayList<>();
 
 	public Task(Execution execution, String taskName, Map<String, Object> parameters) {
 		this.execution = Objects.requireNonNull(execution);
@@ -28,7 +32,7 @@ public final class Task {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	public final boolean equals(Object otherObject) {
@@ -37,11 +41,47 @@ public final class Task {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see java.lang.Object#hashCode()
 	 */
 	public final int hashCode() {
 		return super.hashCode();
 	}
 
+	/**
+	 * This method is invoked when any of the observed tasks change their state.
+	 * The Task itself is given as an argument to the invokation.
+	 * <p>
+	 * When implementing this function be aware of racing conditions.
+	 * There can be multiple threads in this method at once as the notifyObservers functions in Task is usually called by workers.
+	 *
+	 * @param updatedTask the task whose state is changed.
+	 */
+	@Override
+	public void taskUpdated(Task updatedTask) {
+		// TODO update the resolved flag.
+	}
+
+	/**
+	 * Adds an observer to this task.
+	 *
+	 * @param observer observer that will be added to the observers list.
+	 */
+	public void addObserver(TaskObserver observer) {
+		synchronized (this.observers) {
+			observers.add(observer);
+		}
+	}
+
+	/**
+	 * All observers who were added by addObserver are notified by calling taskUpdated using this instance as the argument.
+	 * The order in which observers are added is the same as they are n
+	 */
+	public void notifyObserver() {
+		synchronized (this.observers) {
+			for (TaskObserver obs : this.observers) {
+				obs.taskUpdated(this);
+			}
+		}
+	}
 }

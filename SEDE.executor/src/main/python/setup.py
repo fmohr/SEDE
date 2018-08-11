28af9437d6b19de63f664f3e7d61af144ec3ac03
Amin F <aminfaez@mail.upb.de>
2018-08-11 17:40:50 +0200

    Fixed some issues with gradle build . Now also exports python library

---
+++ SEDE.executor/src/main/python/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='SEDE-executor',
+      version='0.0.3',
+      description='SEDE python executor',
+      url='http://github.com/fmohr/SEDE',
+      author='Amin Faez',
+      author_email='aminfaez@mail.upb.de',
+      license='GNU',
+      long_description_content_type="text/markdown",
+      packages=setuptools.find_packages())

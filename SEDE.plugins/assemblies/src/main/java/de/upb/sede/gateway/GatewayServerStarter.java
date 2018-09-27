bb64e3d88b89ad90f8f74b2272b2c5155b0051e3
Felix Mohr <fmohr@mail.upb.de>
2018-09-27 17:48:17 +0200

    Made some classes serializable and added robustness to some file ops

--- SEDE.plugins/assemblies/src/main/java/de/upb/sede/gateway/GatewayServerStarter.java
+++ SEDE.plugins/assemblies/src/main/java/de/upb/sede/gateway/GatewayServerStarter.java
@@ -21,21 +21,17 @@ import de.upb.sede.webinterfaces.server.StdShellCommands;
 public class GatewayServerStarter {
 	private final static Logger logger = LoggerFactory.getLogger(GatewayServerStarter.class);
 
-
-
 	public static void main(String[] args) throws InterruptedException {
 
-		if(args == null || args.length ==0) {
+		if (args == null || args.length == 0) {
 			throw new RuntimeException("Need at least 1 input argument: port");
 		}
 		int serverPort = Integer.parseInt(args[0]);
-		
+
 		List<String> configsToBeLoaded = new ArrayList<>(Arrays.asList(args));
 		configsToBeLoaded.remove(0); // remove the port
 
-
 		logger.info("Starting gateway with: \nport:{}\nconfigurations:{}", serverPort, configsToBeLoaded);
-
 
 		ClassesConfig classesConfig = new ClassesConfig();
 		OnthologicalTypeConfig typeConfig = new OnthologicalTypeConfig();
@@ -68,42 +64,55 @@ public class GatewayServerStarter {
 		PluginUtil.enablePlugin_TellnetServer(terminalListener);
 	}
 
-
 	static void loadDefaultConfigs(ClassesConfig classesConfig, OnthologicalTypeConfig typeConfig,
-										   List<String> configsToBeLoaded) {
-		for(String configuration : configsToBeLoaded) {
+			List<String> configsToBeLoaded) {
+		for (String configuration : configsToBeLoaded) {
+			
+			File configFile = new File(configuration).getAbsoluteFile();
+			if (!configFile.exists()) {
+				logger.error("Configuration file '{}' associated with config string '{}' does not exist, skipping it.", configFile.getAbsolutePath(), configuration);
+				continue;
+			}
+			
 			/*
-				look for json files ending with classconf.json or typeconf.json in the given list
-				and try to append them to the class/type configuration:
+			 * look for json files ending with classconf.json or typeconf.json in the given
+			 * list and try to append them to the class/type configuration:
 			 */
-			if(configuration.matches("(.*?)classconf\\.json$")) {
-				try{
+			if (configuration.matches("(.*?)classconf\\.json$")) {
+				try {
 					classesConfig.appendConfigFromFiles(configuration);
-					logger.info("Added classes config from: {}" , configuration);
-				}
-				catch(Exception ex) {
+					logger.info("Added classes config from: {}", configuration);
+				} catch (Exception ex) {
 					logger.error("Problem loading class configuration from {}:\n", configuration, ex);
 				}
-			} else if(configuration.matches("(.*?)typeconf\\.json$")) {
+			} else if (configuration.matches("(.*?)typeconf\\.json$")) {
 				try {
 					typeConfig.appendConfigFromFiles(configuration);
 					logger.info("Added type config from: {}", configuration);
 				} catch (Exception ex) {
 					logger.error("Problem loading type configuration from {}:\n", configuration, ex);
 				}
-			} else{
-				logger.info("Config file '{}' doesn't end with classconf.json or typeconf.json.", configuration);
-				if(new File(configuration).isDirectory()) {
-					/*
-					 * load every class conf file  from the given folder:
-					 */
-					logger.info("Loading configurations from '{}'.", configuration);
-					String configFolder = FileUtil.getDirPath(configuration);
-					List<String> additionalConfigs = FileUtil.listAllFilesInDir(configFolder, "(.*?)\\.json$")
-							.stream().map(s-> configFolder+"/"+s) // prepend the folder:
-							.collect(Collectors.toList());
-					loadDefaultConfigs(classesConfig, typeConfig, additionalConfigs);
+			} else {
+				
+				/* check that this configuration specifies a folder */
+				logger.info(
+						"Config file '{}' doesn't end with classconf.json or typeconf.json. Now treating it as a directory ...",
+						configuration);
+				if (!configFile.isDirectory()) {
+					logger.error(
+							"Config '{}' is not a directory. Don't know how to handle it, canceling the load procedure.",
+							configFile.getAbsolutePath());
+					return;
 				}
+				/*
+				 * load every class conf file from the given folder:
+				 */
+				logger.info("Loading configurations from '{}'.", configuration);
+				List<String> additionalConfigs = FileUtil.listAllFilesInDir(configFile.getAbsolutePath(), "(.*?)\\.json$").stream()
+						.map(s -> configFile + "/" + s) // prepend the folder:
+						.collect(Collectors.toList());
+				loadDefaultConfigs(classesConfig, typeConfig, additionalConfigs);
+
 			}
 		}
 	}
--- SEDE.plugins/assemblies/src/main/java/de/upb/sede/gateway/Monolith.java
+++ SEDE.plugins/assemblies/src/main/java/de/upb/sede/gateway/Monolith.java
@@ -1,17 +1,14 @@
 package de.upb.sede.gateway;
 
+import static de.upb.sede.gateway.GatewayServerStarter.loadDefaultConfigs;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import de.upb.sede.config.ClassesConfig;
 import de.upb.sede.config.ExecutorConfiguration;
 import de.upb.sede.config.OnthologicalTypeConfig;
-import de.upb.sede.exec.Executor;
 import de.upb.sede.exec.ExecutorHttpServer;
-import de.upb.sede.exec.ExecutorServerStarter;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static de.upb.sede.gateway.GatewayServerStarter.loadDefaultConfigs;
 
 public class Monolith {
 
@@ -21,14 +18,14 @@ public class Monolith {
 		int javaExecutorPort = 6001;
 
 		List<String> configsToBeLoaded = new ArrayList<>();
-		configsToBeLoaded.add("./deploy/run-gateway-template/configs");
+		configsToBeLoaded.add("deploy/run-gateway-template/configs");
 
 		ClassesConfig classesConfig = new ClassesConfig();
 		OnthologicalTypeConfig typeConfig = new OnthologicalTypeConfig();
 		loadDefaultConfigs(classesConfig, typeConfig, configsToBeLoaded);
 
 		GatewayHttpServer httpGateway = new GatewayHttpServer(gatewayport, classesConfig, typeConfig);
-
+		
 		String executorConfig = "./deploy/executor_configs/all_java_config.json";
 		ExecutorConfiguration executorConfiguration = ExecutorConfiguration.parseJSONFromFile(executorConfig);
 

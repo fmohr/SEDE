eadb6dd37244532ac4a71894cec8b0115b951baf
Amin F <aminfaez@mail.upb.de>
2018-11-21 15:15:05 +0100

    Added bash to gateway docker

--- SEDE.plugins/http_/src/main/java/de/upb/sede/webinterfaces/server/SunHttpHandler.java
+++ SEDE.plugins/http_/src/main/java/de/upb/sede/webinterfaces/server/SunHttpHandler.java
@@ -23,9 +23,10 @@ public class SunHttpHandler implements HttpHandler {
 	public void handle(HttpExchange httpExchange) throws IOException {
 		try {
 			HTTPServerResponse response = serverResponder.get();
-			httpExchange.sendResponseHeaders(200, 0);
 			httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-			httpExchange.getResponseHeaders().add("Content-Type", "text/plain; charset=UTF-8");
+			httpExchange.getResponseHeaders().add("Content-Type", "text/plain");
+			httpExchange.getResponseHeaders().add("Content-Type", "charset=UTF-8");
+			httpExchange.sendResponseHeaders(200, 0);
 			String path = httpExchange.getRequestURI().getPath();
 			response.receive(Optional.of(path), httpExchange.getRequestBody(), httpExchange.getResponseBody());
 		} catch(RuntimeException ex) {

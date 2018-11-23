537d769b4ef74e8925b4153b638869f39513be0d
Achim Loesch <achim.loesch@upb.de>
2018-11-23 13:19:11 +0100

    Add an example for a resource-bound service.

---
+++ CServices/src/main/java/C2Services/C2Service_CPU_grey.java
@@ -0,0 +1,17 @@
+package C2Services;
+
+import C2Data.C2Image;
+import C2Data.C2Resource;
+
+public class C2Service_CPU_grey extends C2Service_grey {
+    private C2Resource mResource;
+    public C2Service_CPU_grey() {
+        super();
+
+        mResource = new C2Resource("cpu");
+    }
+
+    public C2Image processImage(C2Image sourceImage) {
+        return super.processImage(mResource, sourceImage);
+    }
+}

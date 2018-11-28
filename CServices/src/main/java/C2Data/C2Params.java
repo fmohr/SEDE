b35cd49a797d5ed541c08c578a9be196918105aa
Nabanita Das <nabanita@campus.uni-paderborn.degit config --global user.email nabanita@campus.uni-paderborn.de>
2018-11-28 16:34:45 +0100

    added java classes for supporting params

---
+++ CServices/src/main/java/C2Data/C2Params.java
@@ -0,0 +1,18 @@
+package C2Data;
+
+public class C2Params {
+    private double mParam;
+
+    public C2Params(double paramValue)
+    {
+        mParam = paramValue;
+    }
+
+    public double getmParam() {
+        return mParam;
+    }
+
+    public void setmParam(double mParam) {
+        this.mParam = mParam;
+    }
+}

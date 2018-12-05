9996bfb79603f51915669e781bbe82c5237a3612
Nabanita Das <nabanita@campus.uni-paderborn.degit config --global user.email nabanita@campus.uni-paderborn.de>
2018-12-05 15:44:51 +0100

    added gaussian low pass functionality

---
+++ CServices/csrc/service_plugins/service_gausslowpass/service_gausslowpass_cpu.c
@@ -0,0 +1,104 @@
+/* service_gausslowpass_cpu.c */
+
+#include "service_gausslowpass.h"
+
+int32_t run_service_gausslowpass_cpu(PixelPacket *pixpack_target, PixelPacket *pixpack_source, int32_t rows, int32_t columns) {
+ 	int32_t temp_red = 0, temp_green = 0, temp_blue = 0;
+	int32_t i, j, pos;
+	
+	#pragma omp parallel
+	{
+		#pragma omp for private(j, pos, temp_red, temp_green, temp_blue)
+		for (i=2; i<rows-2; ++i) {
+			for (j=2; j<columns-2; ++j) {
+				pos = i*columns+j;
+				temp_red	=	   pixpack_source[pos-(2*columns)-2].red
+							+	 5*pixpack_source[pos-(2*columns)-1].red
+							+	 7*pixpack_source[pos-(2*columns)  ].red
+							+	 5*pixpack_source[pos-(2*columns)+1].red
+							+	   pixpack_source[pos-(2*columns)+2].red;
+				temp_green	=	   pixpack_source[pos-(2*columns)-2].green
+							+	 5*pixpack_source[pos-(2*columns)-1].green
+							+	 7*pixpack_source[pos-(2*columns)  ].green
+							+	 5*pixpack_source[pos-(2*columns)+1].green
+							+	   pixpack_source[pos-(2*columns)+2].green;
+				temp_blue	=	   pixpack_source[pos-(2*columns)-2].blue
+							+	 5*pixpack_source[pos-(2*columns)-1].blue
+							+	 7*pixpack_source[pos-(2*columns)  ].blue
+							+	 5*pixpack_source[pos-(2*columns)+1].blue
+							+	   pixpack_source[pos-(2*columns)+2].blue;
+				
+				temp_red	+=	 5*pixpack_source[pos-(  columns)-2].red
+							+	20*pixpack_source[pos-(  columns)-1].red
+							+	33*pixpack_source[pos-(  columns)  ].red
+							+	20*pixpack_source[pos-(  columns)+1].red
+							+	 5*pixpack_source[pos-(  columns)+2].red;
+				temp_green	+=	 5*pixpack_source[pos-(  columns)-2].green
+							+	20*pixpack_source[pos-(  columns)-1].green
+							+	33*pixpack_source[pos-(  columns)  ].green
+							+	20*pixpack_source[pos-(  columns)+1].green
+							+	 5*pixpack_source[pos-(  columns)+2].green;
+				temp_blue	+=	 5*pixpack_source[pos-(  columns)-2].blue
+							+	20*pixpack_source[pos-(  columns)-1].blue
+							+	33*pixpack_source[pos-(  columns)  ].blue
+							+	20*pixpack_source[pos-(  columns)+1].blue
+							+	 5*pixpack_source[pos-(  columns)+2].blue;
+				
+				temp_red	+=	 7*pixpack_source[pos-2].red
+							+	33*pixpack_source[pos-1].red
+							+	55*pixpack_source[pos  ].red
+							+	33*pixpack_source[pos+1].red
+							+	 7*pixpack_source[pos+2].red;
+				temp_green	+=	 7*pixpack_source[pos-2].green
+							+	33*pixpack_source[pos-1].green
+							+	55*pixpack_source[pos  ].green
+							+	33*pixpack_source[pos+1].green
+							+	 7*pixpack_source[pos+2].green;
+				temp_blue	+=	 7*pixpack_source[pos-2].blue
+							+	33*pixpack_source[pos-1].blue
+							+	55*pixpack_source[pos  ].blue
+							+	33*pixpack_source[pos+1].blue
+							+	 7*pixpack_source[pos+2].blue;
+				
+				temp_red	+=	 5*pixpack_source[pos+(  columns)-2].red
+							+	20*pixpack_source[pos+(  columns)-1].red
+							+	33*pixpack_source[pos+(  columns)  ].red
+							+	20*pixpack_source[pos+(  columns)+1].red
+							+	 5*pixpack_source[pos+(  columns)+2].red;
+				temp_green	+=	 5*pixpack_source[pos+(  columns)-2].green
+							+	20*pixpack_source[pos+(  columns)-1].green
+							+	33*pixpack_source[pos+(  columns)  ].green
+							+	20*pixpack_source[pos+(  columns)+1].green
+							+	 5*pixpack_source[pos+(  columns)+2].green;
+				temp_blue	+=	 5*pixpack_source[pos+(  columns)-2].blue
+							+	20*pixpack_source[pos+(  columns)-1].blue
+							+	33*pixpack_source[pos+(  columns)  ].blue
+							+	20*pixpack_source[pos+(  columns)+1].blue
+							+	 5*pixpack_source[pos+(  columns)+2].blue;
+				
+				temp_red	+=	   pixpack_source[pos+(2*columns)-2].red
+							+	 5*pixpack_source[pos+(2*columns)-1].red
+							+	 7*pixpack_source[pos+(2*columns)  ].red
+							+	 5*pixpack_source[pos+(2*columns)+1].red
+							+	   pixpack_source[pos+(2*columns)+2].red;
+				temp_green	+=	   pixpack_source[pos+(2*columns)-2].green
+							+	 5*pixpack_source[pos+(2*columns)-1].green
+							+	 7*pixpack_source[pos+(2*columns)  ].green
+							+	 5*pixpack_source[pos+(2*columns)+1].green
+							+	   pixpack_source[pos+(2*columns)+2].green;
+				temp_blue	+=	   pixpack_source[pos+(2*columns)-2].blue
+							+	 5*pixpack_source[pos+(2*columns)-1].blue
+							+	 7*pixpack_source[pos+(2*columns)  ].blue
+							+	 5*pixpack_source[pos+(2*columns)+1].blue
+							+	   pixpack_source[pos+(2*columns)+2].blue;
+				
+				pixpack_target[pos].red		= temp_red		/ 339;
+				pixpack_target[pos].green	= temp_green	/ 339;
+				pixpack_target[pos].blue	= temp_blue		/ 339;
+				pixpack_target[pos].opacity	= 0;
+			}
+		}
+	}
+	
+	return 0;
+}
--- CServices/src/main/java/C2Services/C2Service_JAVA_sobel.java
+++ CServices/src/main/java/C2Services/C2Service_JAVA_sobel.java
@@ -15,26 +15,6 @@ public class C2Service_JAVA_sobel extends C2Service_grey {
         mResource = new C2Resource("java");
     }
 
-    public void setOptions(C2Params params){
-
-        double height = 0;
-        double width = 0;
-
-        Map<String, Double> paramMap    = params.getParams();
-        Set<String> paramKeys           = paramMap.keySet();
-
-        for (String key : paramKeys) {
-            double paramValue1 = paramMap.get("height");
-            height = paramValue1;
-            double paramValue2 = paramMap.get("width");
-            width = paramValue2;
-        }
-
-        System.out.println("Height:" + height );
-        System.out.println("Width:" + width );
-
-    }
-
     public C2Image processImage(C2Image sourceImage) {
         return super.processImage(mResource, sourceImage);
     }

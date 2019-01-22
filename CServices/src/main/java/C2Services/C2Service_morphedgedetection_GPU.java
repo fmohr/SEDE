8d2392e3feece5a92a816e0f9dbbe5a4ef9db9b4
Nabanita Das <nabanita@campus.uni-paderborn.degit config --global user.email nabanita@campus.uni-paderborn.de>
2019-01-22 15:29:29 +0100

    added classes for GPU and FPGA

---
+++ CServices/src/main/java/C2Services/C2Service_morphedgedetection_GPU.java
@@ -0,0 +1,21 @@
+package C2Services;
+
+import C2Data.C2Image;
+import C2Data.C2Params;
+import C2Data.C2Resource;
+
+import java.util.Map;
+import java.util.Set;
+
+public class C2Service_morphedgedetection_GPU extends C2Service_morphedgedetection{
+	private C2Resource mResource;
+	public C2Service_morphedgedetection_GPU() {
+		super();
+
+		mResource = new C2Resource("gpu");
+	}
+
+	public C2Image processImage(C2Image sourceImage) {
+		return super.processImage(mResource, sourceImage);
+	}
+}

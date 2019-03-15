5778dd157d870d8711c1ffd52237761c8d6503f0
Nabanita Das <nabanita@cc-8.pc2.uni-paderborn.de>
2019-03-15 11:11:38 +0100

    added test case for canny

--- SEDE.services/c2.image/src/test/java/demo/C2ServiceTests.java
+++ SEDE.services/c2.image/src/test/java/demo/C2ServiceTests.java
@@ -298,4 +298,71 @@ public class C2ServiceTests {
 		JOptionPane.showMessageDialog(null, lenna_mod.convertToImageIcon(), "Morph Edge Detected Image", JOptionPane.PLAIN_MESSAGE);
 
 	}
+
+
+	@Test
+	public void testGreyGaussCanny() throws InvocationTargetException, InterruptedException {
+
+		String composition =
+				"s1 = C2Services.C2Service_grey::__construct();\n" +
+						"imageInter1 = s1::processImage({i1=resource1, i2=imageIn});\n" +
+						"s2 = C2Services.C2Service_gausslowpass::__construct();\n" +
+						"imageInter2 = s2::processImage({i1=resource2, i2=imageInter1});\n" +
+						"s3 = C2Services.C2Service_canny::__construct();\n" +
+						"s3::setOptions({i1=paramValueCanny});\n" +
+						"imageOut = s3::processImage({i1=resource3, i2=imageInter2});\n";
+
+		C2Resource SCPU	= new C2Resource("scpu");
+		C2Resource CPU	= new C2Resource("cpu");
+		C2Resource GPU	= new C2Resource("gpu");
+		C2Resource FPGA	= new C2Resource("fpga");
+		C2Resource JAVA	= new C2Resource("java");
+		C2Resource OVERLAY = new C2Resource("overlay");
+
+
+
+		Map< String,Double> paramsCanny = new HashMap< String,Double>();
+		paramsCanny.put("rows",3.0);
+		paramsCanny.put("cols",3.0);
+		paramsCanny.put("Thigh",3.0);
+		paramsCanny.put("Tlow",3.0);
+
+
+		C2Params paramValuesCanny = new C2Params(paramsCanny);
+
+		SEDEObject inputObject_lenna	= new ObjectDataField(C2Image.class.getName(), lenna);
+		SEDEObject inputObject_res1		= new ObjectDataField(C2Resource.class.getName(), SCPU);
+		SEDEObject inputObject_res2		= new ObjectDataField(C2Resource.class.getName(), OVERLAY);
+		SEDEObject inputObject_res3		= new ObjectDataField(C2Resource.class.getName(), GPU);
+		SEDEObject inputObject_paramCanny	= new ObjectDataField(C2Params.class.getName(), paramValuesCanny);
+
+		ResolvePolicy policy = new ResolvePolicy();
+		policy.setServicePolicy("None");
+		policy.setReturnFieldnames(Arrays.asList("imageInter1", "imageInter2", "imageOut"));
+
+		Map<String, SEDEObject> inputs = new HashMap<>();
+		inputs.put("imageIn", inputObject_lenna);
+		inputs.put("resource1", inputObject_res1);
+		inputs.put("resource2", inputObject_res2);
+		inputs.put("resource3", inputObject_res3);
+		inputs.put("paramValueCanny", inputObject_paramCanny);
+
+		RunRequest runRequest = new RunRequest("proc_cservices", composition, policy, inputs);
+
+		Map<String, Result> resultMap = coreClient.blockingRun(runRequest);
+		Result inter1 = resultMap.get("imageInter1");
+		Result inter2 = resultMap.get("imageInter2");
+		Result result = resultMap.get("imageOut");
+
+		C2Image lenna_inter1 = inter1.castResultData(C2Image.class.getName(), C2ImageCaster.class).getDataField();
+		C2Image lenna_inter2 = inter2.castResultData(C2Image.class.getName(), C2ImageCaster.class).getDataField();
+		lenna_mod = result.castResultData(C2Image.class.getName(), C2ImageCaster.class).getDataField();
+
+		JOptionPane.showMessageDialog(null, lenna.convertToImageIcon(), "Input Image", JOptionPane.PLAIN_MESSAGE);
+		JOptionPane.showMessageDialog(null, lenna_inter1.convertToImageIcon(), "Grey Image", JOptionPane.PLAIN_MESSAGE);
+		JOptionPane.showMessageDialog(null, lenna_inter2.convertToImageIcon(), "Gauss Blurred Image", JOptionPane.PLAIN_MESSAGE);
+		JOptionPane.showMessageDialog(null, lenna_mod.convertToImageIcon(), "Canny  Filtered Image", JOptionPane.PLAIN_MESSAGE);
+	}
+
+
 }
